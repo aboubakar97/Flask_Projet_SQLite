5ac435665538889961ce27df77f15e96da8f7ada
aboubakar97 <aboubakarpp@gmail.com>
2025-03-19 15:19:51 +0100

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -8,12 +8,12 @@ with open('schema2.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO livres (id, created, auteur, titre,annee_publication, genre,stock) VALUES (?, ?,?, ?,?,?,?)",(1, '2025-03-19 10:00:00', 'Le Petit Prince',"Antoine de Saint-Exupéry", 1943, 'Conte philosophique', 5))
-cur.execute("INSERT INTO livres (id, created, auteur, titre,annee_publication, genre,stock) VALUES (?, ?,?, ?,?,?,?)",(2, '2025-0-19 10:05:00', '1984', "George Orwell", 1949, 'Science-fiction', 3))
-cur.execute("INSERT INTO livres (id, created, auteur, titre,annee_publication, genre,stock) VALUES (?, ?,?, ?,?,?,?)",(3, '2025-03-19 10:10:00', 'L’Étranger',"Albert Camus", 1942, 'Roman', 4))
-cur.execute("INSERT INTO livres (id, created, auteur,titre,annee_publication, genre,stock) VALUES (?, ?, ?,?,?,?,?)",(4, '2025-03-19 10:15:00', 'Moby Dick',"Herman Melville", 1851, 'Aventure', 2))
-cur.execute("INSERT INTO livres (id, created, auteur,titre,annee_publication, genre,stock) VALUES (?, ?, ?,?,?,?,?)",(5, '2025-03-19 10:20:00', 'Pride and Prejudice',"Jane Austen", 1813, 'Romance', 6))
-cur.execute("INSERT INTO livres (id, created, auteur,titre,annee_publication, genre,stock) VALUES (?, ?, ?,?,?,?,?)",(6, '2025-03-19 10:25:00', 'The Great Gatsby',"Scott Fitzgerald, F.", 1925, 'Roman', 7))
+cur.execute("INSERT INTO livres (id, created,  titre,auteur,annee_publication, genre,stock) VALUES (?, ?,?, ?,?,?,?)",(1, '2025-03-19 10:00:00', 'Le Petit Prince',"Antoine de Saint-Exupéry", 1943, 'Conte philosophique', 5))
+cur.execute("INSERT INTO livres (id, created,  titre,auteur,annee_publication, genre,stock) VALUES (?, ?,?, ?,?,?,?)",(2, '2025-0-19 10:05:00', '1984', "George Orwell", 1949, 'Science-fiction', 3))
+cur.execute("INSERT INTO livres (id, created,  titre,auteur,annee_publication, genre,stock) VALUES (?, ?,?, ?,?,?,?)",(3, '2025-03-19 10:10:00', 'L’Étranger',"Albert Camus", 1942, 'Roman', 4))
+cur.execute("INSERT INTO livres (id, created, titre,auteur,annee_publication, genre,stock) VALUES (?, ?, ?,?,?,?,?)",(4, '2025-03-19 10:15:00', 'Moby Dick',"Herman Melville", 1851, 'Aventure', 2))
+cur.execute("INSERT INTO livres (id, created, titre,auteur,annee_publication, genre,stock) VALUES (?, ?, ?,?,?,?,?)",(5, '2025-03-19 10:20:00', 'Pride and Prejudice',"Jane Austen", 1813, 'Romance', 6))
+cur.execute("INSERT INTO livres (id, created, titre,auteur,annee_publication, genre,stock) VALUES (?, ?, ?,?,?,?,?)",(6, '2025-03-19 10:25:00', 'The Great Gatsby',"Scott Fitzgerald, F.", 1925, 'Roman', 7))
 
 connection.commit()
 connection.close()
