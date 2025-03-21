88d67675d29938fb2278599c941fa1e8507fead3
aboubakar97 <aboubakarpp@gmail.com>
2025-03-21 12:03:46 +0100

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -15,6 +15,14 @@ cur.execute("INSERT INTO livres (titre,auteur,annee_publication, genre,stock) VA
 cur.execute("INSERT INTO livres  (titre,auteur,annee_publication, genre,stock) VALUES ( ?,?,?,?,?)",( 'Pride and Prejudice',"Jane Austen", 1813, 'Romance', 6))
 cur.execute("INSERT INTO livres (titre,auteur,annee_publication, genre,stock) VALUES ( ?,?,?,?,?)",( 'The Great Gatsby',"Scott Fitzgerald, F.", 1925, 'Roman', 7))
 
+with open('schema2.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO utilisateurs (nom,prenom,email,mot_de_passe,role) VALUES (?, ?,?,?,?)",( 'Dupont',	'Jean',	'jean.dupont@email.com'	,'123456','Utilisateur'))
+cur.execute("INSERT INTO utilisateurs (nom,prenom,email,mot_de_passe,role) VALUES (?, ?,?,?,?)",( 'Rousseau',	'Emma',	'emma.rousseau@email.com',	'123456',	'Administrateur'))
+
 connection.commit()
 connection.close()
 
